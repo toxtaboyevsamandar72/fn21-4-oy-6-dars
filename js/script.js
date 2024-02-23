ef95e04de3a05859fcdcae5e762b661ec398e423
toxtaboyevsamandar72 <toxtaboyevsamandar720@gmail.com>
2024-02-23 20:55:08 +0500

    index html

---
+++ js/script.js
@@ -0,0 +1,44 @@
+
+// console.log(Math);
+// console.log(Math.E);
+// console.log(Math.PI);
+
+// let area = 7.3;
+// console.log(Math.round(area));
+
+// console.log(Math.floor(area));
+
+// console.log(Math.ceil(area));
+
+// console.log(Math.trunc(area));
+
+
+// console.log(Math.trunc(Math.random()*10));
+
+
+// let Namber = "Samandar"
+
+// console.log(Namber.toUpperCase());
+
+// console.log(Namber.toLowerCase());
+
+
+// let email = '  samandartoxtaboyev@gmail.com   '
+
+// console.log(email.indexOf("a"));
+
+// console.log(email.lastIndexOf("a"));
+
+
+// console.log(email.slice(0,1));
+
+// console.log(email.substr(4, 12));
+
+
+// console.log(email.replace('a','d'));
+
+
+// console.log(email.charAt());
+
+
+// console.log(email.trim());
